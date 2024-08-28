1cfbb50f1ff01fb6b4a477f8c4a77fbf79f05417
aikdp <kdprasad028@gmail.com>
2024-08-28 11:35:17 +0530

    expense shell mysqld db installing

--- log-col-exp-shell.sh
+++ log-col-exp-shell.sh
@@ -30,7 +30,7 @@ CHECK(){
     fi    
 }
 
-dnf list installed mysql | tee -a $LOG_FILE
+dnf list installed mysql-server | tee -a $LOG_FILE
 if [ $? -eq 0 ]
 then 
     echo -e "$Y MYSQL Server is already installed.$N"
