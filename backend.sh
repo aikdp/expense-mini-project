64a1fa22d8b1be4f37b1d64550c9fc900fc43077
aikdp <kdprasad028@gmail.com>
2024-08-28 19:01:05 +0530

    expense backend server installing

--- backend.sh
+++ backend.sh
@@ -17,77 +17,77 @@ N="\e[0m"
 ROOT(){
     if [ $USERID -eq 0 ]
     then
-        echo -e "USER has ROOT Access."
+        echo -e "$G USER has ROOT Access.$N" | tee -a $LOG_FILE
     else
-        echo -e "USER does not have root access..Please login as ROOT USER"
+        echo -e "USER does not have root access..Please login as ROOT USER" | tee -a $LOG_FILE
         exit 1
     fi            
 }
 CHECK(){
     if [ $1 -eq 0 ]
     then
-        echo "$2 is...SUCCESS"
+        echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo "$2 is ...FAILED"
+        echo -e "$2 is ...$R FAILED $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 ROOT
 
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE
 CHECK $? "DISABLE Nodejs modules"
 
-dnf module enable nodejs:20 -y
-CHECK $? "ENABLE Nodejs 20 modules"
+dnf module enable nodejs:20 -y &>>$LOG_FILE 
+CHECK $? "ENABLE Nodejs 20 module"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE
 CHECK $? "INSTALLING nodejs"
 
-id expense
+id expense &>>$LOG_FILE
 if [ $? -eq 0 ]
 then 
-    echo "User expense Already Exists,..SKIP ADDING EXPENSE USER"
+    echo -e "User expense Already Exists,..$Y SKIP ADDING EXPENSE USER $N" | tee -a $LOG_FILE
 else
-    echo "User expense not createdExists,..Please ADD USER EXPENSE"
-    useradd expense
+    echo -e "$R User expense not Exists,..Please ADD USER EXPENSE $N" | tee -a $LOG_FILE
+    useradd expense &>>$LOG_FILE
     CHECK $? "Adding user Expense"
 fi
 
 mkdir -p /app
 CHECK $? "Folder /app is creating"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
-CHECK $? "Download Backend application code"
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+CHECK $? "Downloaded Backend application code"
 
 cd /app
 CHECK $? "Going to /app folder"
 
-rm -rf /app/*
-unzip /tmp/backend.zip
+rm -rf /app/* &>>$LOG_FILE
+unzip /tmp/backend.zip &>>$LOG_FILE
 CHECK $? "unzipping backend appliaction"
 
 cd /app
-npm install
+npm install &>>$LOG_FILE
 CHECk $? "Installing Dependcies of Backend application"
 
-cp /home/ec2-user/expense-backend/backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOG_FILE
 CHECK $? "Copied backend service for API call to DB"
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOG_FILE
 CHECK $? "Installing MYSQL"
 
-mysql -h mysql.telugudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.telugudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 CHECK $? "Loading SCHEMA to backend.sql"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG_FILE
 CHECK $? "DEAMON relaod"
 
-systemctl start backend
+systemctl start backend &>>$LOG_FILE
 CHECK $? "START Backend"
 
-systemctl enable backend
+systemctl enable backend &>>$LOG_FILE
 CHECK $? "Enabling Backend"
 
-systemctl restart backend
+systemctl restart backend &>>$LOG_FILE
 CHECK $? "RESTART Backend SERVICE"
