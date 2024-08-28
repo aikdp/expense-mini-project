3f587addaa22a638a7e25d7bed1ea7da08f7169e
aikdp <kdprasad028@gmail.com>
2024-08-28 11:16:51 +0530

    expense shell mysqld db installing

---
+++ log-col-exp-shell.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+mkdir -p $LOG_FOLDER
+LOG_FOLDER=$(/var/log/expense)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME.log"
+
+if [ $USERID -eq 0 ] 
+then 
+    echo -e "USER have a $G ROOT ACCESS $N" 
+else
+    echo -e "USER does not have ROOT ACCESS, $R Please LOGIN as a ROOT USER $N"
+    exit 1
+fi
+
+CHECK(){
+    if [ $1 -eq  0 ]
+    then 
+        echo -e "$2 is $G SUCCESS $N"
+    else
+        echo -e "$2 is $R FAILED $N"
+        exit 1
+    fi    
+}
+
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -eq 0 ]
+then 
+    echo -e "$Y MYSQL Server is already installed.$N"
+else
+    echo -e "MYSQL Server is not Installed, $R Please install MSQL $N"
+    dnf install mysql-server -y &>>$LOG_FILE
+    CHECK $? "MYSQL installation"  
+fi    
+
+systemctl enable mysqld &>>$LOG_FILE
+    CHECK $? "ENABLING MYSQL Service"
+ 
+systemctl start mysqld &>>$LOG_FILE
+    CHECK $? "STARTING MYSQL Service"
+
+mysql -h mysql.telugudevops.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+   if [ $? -eq 0 ]
+   then 
+        echo -e "$Y ROOT Password is already SETUP,,SKIPPING $N"
+    else 
+        echo -e "ROOT Password is not setup,,,$R Please setup MYSQL ROOT Password $N"
+        mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
+        CHECK $? "ROOT PASSWORD SETUP"
+    fi    
