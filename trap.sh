e77c04fc70f69dc044a57f0f7b297148e4b925c0
aikdp <kdprasad028@gmail.com>
2024-08-29 19:12:33 +0530

    Practicing SET commands for exit status

---
+++ trap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "Failed at: $1 : $2"
+}
+
+tarp 'failure "${LINENO}" "$BASH_COMMAND"' ERR
+
+echo "Hi Prasad"
+
+echhooo "Hi Durga, How are you prasad"
+
+echo "DOing graet. How about you Durga"
