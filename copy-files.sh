dd329f5458df40b830c53864c8e21ba5dbe20920
aikdp <kdprasad028@gmail.com>
2024-08-29 18:29:28 +0530

    working expense mini project with shell automation

--- copy-files.sh
+++ copy-files.sh
@@ -12,6 +12,6 @@ DEST_DIR=$(/home/ec2-user/prasad)
 # if [ -d $SOURCE_DIR ]
 # then
 #     echo "enter source dirctory path: $SOURCE_DIR"
-cp $SOURCE_DIR $DEST_DIR
+cp -f $SOURCE_DIR $DEST_DIR
 
 # echo "Copying files from source dir to Destination DIR is started at: $(date)" 
