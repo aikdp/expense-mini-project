3c8bdf71b5c6e3ccfd4ca6b42a5cf891f4a8999e
aikdp <kdprasad028@gmail.com>
2024-08-29 19:03:15 +0530

    Practicing SET commands for exit status

--- setcommand.sh
+++ setcommand.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# #Checking commands if we enter wrong command also execution not stopped
+# echo "Hello world"
+
+# echooo "heloo world ..Failure"
+
+# echo "Hello world after failure"
+
+#Checking commands if we enter wrong command, execution should stop
+set -e 
 echo "Hello world"
 
 echooo "heloo world ..Failure"
