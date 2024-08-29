398d9ae10355b3dcb9aaaec688f79367f1f6ed80
aikdp <kdprasad028@gmail.com>
2024-08-29 18:32:34 +0530

    working expense mini project with shell automation

--- copy-files.sh
+++ copy-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-SOURCE_DIR=$(/home/ec2-user/durga)
-DEST_DIR=$(/home/ec2-user/prasad)
-
+SOURCE_DIR=$1
+DEST_DIR=$2
 
 # USAGE(){
 #     echo "USAGE:: is please give source directoy path and Destination Dir path"
@@ -12,6 +11,6 @@ DEST_DIR=$(/home/ec2-user/prasad)
 # if [ -d $SOURCE_DIR ]
 # then
 #     echo "enter source dirctory path: $SOURCE_DIR"
-cp -f $SOURCE_DIR $DEST_DIR
+cp -t $SOURCE_DIR $DEST_DIR
 
 # echo "Copying files from source dir to Destination DIR is started at: $(date)" 
