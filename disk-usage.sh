07dfad5c95426886b3cee9ef19c4046cab6fd245
aikdp <kdprasad028@gmail.com>
2024-08-31 20:20:43 +0530

    Shell practcing

--- disk-usage.sh
+++ disk-usage.sh
@@ -4,6 +4,10 @@
 DU=$(df -hT | grep xfs)
 DT=5 
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 while IFS= read -r line
 do
@@ -11,6 +15,6 @@ do
     DIR_NAME=$(echo $line | grep xfs | awk -F " " '{print $NF}') 
     if [ "$U" -ge $DT ]
     then
-        echo "$DIR_NAME disk usage is more than $DT, Current Usage Value is: $U...PLEASE CHECK"
+        echo -e "$Y $DIR_NAME $N disk usage is more than $DT, $Y Current Usage Value is $N : $U...$R PLEASE CHECK $N"
     fi
 done <<< $DU
