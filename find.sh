97f9dd1e0249042e23e37c7fb095b393b9e15c3a
aikdp <kdprasad028@gmail.com>
2024-08-31 14:29:23 +0530

    Shell practcing

--- find.sh
+++ find.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #list of all files in Source dirctory
-SOURCE_DIR=/home/ec2-user/logs
+SOURCE_DIR=/home/ec2-user/app-logs
 
-FILES=$(find $SOURCE_DIR -type f)
+# FILES=$(find $SOURCE_DIR -type f)
+# FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+FILES=$(find $SOURCE_DIR -type f -name "*.log")
 echo "list of files are: $FILES"
