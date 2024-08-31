a3d0d3b068e93588b6f9aea34d6c96d022cb517b
aikdp <kdprasad028@gmail.com>
2024-08-31 14:35:43 +0530

    Shell practcing

--- find.sh
+++ find.sh
@@ -5,5 +5,5 @@ SOURCE_DIR=/home/ec2-user/app-logs
 
 # FILES=$(find $SOURCE_DIR -type f)
 # FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)        #show all .log files older than 14 days
-FILES=$(find $SOURCE_DIR -type f -name "*.log" -mtime +10)             #print all .log files without days means all .log files
+FILES=$(find $SOURCE_DIR -type f -mtime +10)             #print all .log files without days means all .log files
 echo "list of files are: $FILES"
