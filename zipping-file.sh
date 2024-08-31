d7d0518b46cafdb05fe018930b9f949759a27a3c
aikdp <kdprasad028@gmail.com>
2024-08-31 16:27:28 +0530

    Shell practcing

---
+++ zipping-file.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3: -14}
+
+USAGE(){
+    echo  -e "$R USAGE: please give argumnets like sh <filename> <Source DIR> Destionation DIR><days> $N"
+    exit 1
+}
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then 
+    echo -e "$R $SOURCE_DIR not exist $N, please give coreect $SOURCE_DIR Directory"
+    exit 1
+fi
+if [ ! -d $DEST_DIR ]
+then 
+    echo -e "$R $DEST_DIR not exist $N, please give coreect $DEST_DIR Directory"
+    exit 1
+fi
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+echo "List of all log files are: $FILES"
+
+if [ ! -z $FILES ] # not empty files then it will true
+then 
+    echo " Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIME_STAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+    if [ -f $ZIP_FILE ]
+    then 
+        echo "All log files older than 14 dyas are successfully ZIPPED"
+        while IFS= read -r file
+        do
+            echo "Deleting all log files" $file"
+            rm -rf $file
+        done <<< $FILES 
+    else
+        echo ""All log files older than 14 dyas are NOT ZIPPED"
+        exit 1
+    fi
+else
+    echo "Files are NOT FOUND"
+    exit
+fi
