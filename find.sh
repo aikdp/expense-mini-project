ce4e5226e5648066b3c5897b34403237c6eb5b2b
aikdp <kdprasad028@gmail.com>
2024-08-31 14:58:40 +0530

    Shell practcing

--- find.sh
+++ find.sh
@@ -3,8 +3,8 @@
 #list of all files in Source dirctory
 SOURCE_DIR=/home/ec2-user/app-logs
 
-# FILES=$(find $SOURCE_DIR -type f)
+# FILES=$(find $SOURCE_DIR -type f)         #all files in that dirctory
 # FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)        #show all .log files older than 14 days
 # FILES=$(find $SOURCE_DIR -type f -mtime +10)            #print all files older than 10 days
-FILES=$(find $SOURCE_DIR -name "*.log")
+FILES=$(find $SOURCE_DIR -name "*.log")             #all .log files in that dir 
 echo "list of files are: $FILES"
---
+++ remove-files-col.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#deleting logs in source directiry
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=/home/ec2-user/app-logs   #Dynamiclly giving Source Directory
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$Y SOurce diractory exist $N"
+
+else
+    echo -e "$R SOurce Diractory Doesn't exist $N"
+    exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo -e "$Y list of files are:$N $FILES"
+while IFS= read -r file
+do
+    echo -e "$Y Deleting list of files: $N $file"
+    # rm -rf $file
+done <<< $FILES
+
