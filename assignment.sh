2695e95b20691d6b55b42da219dca02e4db5e48b
aikdp <kdprasad028@gmail.com>
2024-09-01 06:24:04 +0530

    Prcticing shell scripting

--- assignment.sh
+++ assignment.sh
@@ -19,4 +19,4 @@ FN=text.txt
 #     fi
 # done < $FN
 
-sed -e 's/[^[:alpha:]]/ /g' $FN | uniq -c | tr '\n' " " | tr -s " " | tr " " '\n' | tr 'A-Z' 'a-z' | sort | sort -nr | head -5
+sed -e 'g/[^[:alpha:]]/ /s' $FN | tr '\n' " " | tr -s " " | tr " " '\n' | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | head -5
