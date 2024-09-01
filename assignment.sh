8a374987ec5c8f0d6b14149926fd959555bfc385
aikdp <kdprasad028@gmail.com>
2024-09-01 06:15:44 +0530

    Prcticing shell scripting

--- assignment.sh
+++ assignment.sh
@@ -6,15 +6,17 @@
 
 FN=text.txt
 
-while IFS= read line
-do
-    if [ ! -z $FN ]
-    then
-        echo "text files is not empty"
-        TOP_OCR=$( cat $FN | uniq -c | sort -nr | head -5)
-        echo "top most frequent words along with counts: $TOP_OCR"
-    else
-        echo "Text file empty, please check"
-        exit 1
-    fi
-done < $FN
+# while IFS= read line
+# do
+#     if [ ! -z $FN ]
+#     then
+#         echo "text files is not empty"
+#         TOP_OCR=$( cat $FN | uniq -c | sort -nr | head -5)
+#         echo "top most frequent words along with counts: $TOP_OCR"
+#     else
+#         echo "Text file empty, please check"
+#         exit 1
+#     fi
+# done < $FN
+
+sed -e 's/[^[:alpha:]]/ /g' $FN | tr '\n' " " | tr -s " " | tr " " '\n' | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | nl
